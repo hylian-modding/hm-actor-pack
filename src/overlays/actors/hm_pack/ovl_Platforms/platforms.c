79f3107dbdf24d8fef87438cb10ff35de6bdf638
CDi <deez@nutz>
2023-11-14 21:16:54 -0600

    Fix platform bobbing behavior

--- src/overlays/actors/hm_pack/ovl_Platforms/platforms.c
+++ src/overlays/actors/hm_pack/ovl_Platforms/platforms.c
@@ -8,40 +8,39 @@
 #include "assets_hm_pack/objects/object_platforms/object_platforms.h"
 
 #define PLATFORM_TYPE(this) ((this->dyna.actor.params >> 0xC) & 0xF) // 0xF000
-#define SWITCH_FLAG(this) (this->dyna.actor.params & 0x3F) // 0x00FF
-#define TIMER_DURATION(this) (this->dyna.actor.home.rot.x) // <= 10 minutes (600)
+#define SWITCH_FLAG(this) (this->dyna.actor.params & 0x3F)           // 0x00FF
+#define TIMER_DURATION(this) (this->dyna.actor.home.rot.x)           // <= 10 minutes (600)
 
 /**
  * Params:
  *  0xF000 = Type
- *     0x0 = Square Wooden Platform 
+ *     0x0 = Square Wooden Platform
  *     0x1 = Square Wooden Platform (Checkered Pattern)
  *     0x2 = Square Stone Platform
  *     0x3 = Square Stone Platform (Checkered Pattern)
- *     0x4 = Square Grass Platform 
+ *     0x4 = Square Grass Platform
  *     0x5 = Square Ice Platform
  *     0x6 = Hexagonal Grass Platform
  *     0x7 = Hexagonal Ice Platform
  *     0x8 = Cone Floating Grass Platform
- * 
+ *
  *  0x00FF = Switch Flag
- *  
+ *
  *  Rot X > 0 = Duration timer for spawned platforms (in seconds)
- * 
- *  All platform types (except for the Cone Floating Type) will spawn on switch flag activation. After the specified timer amount, they unset the switch
- *  and disappear again until the flag is once again set.
- *  
+ *
+ *  All platform types (except for the Cone Floating Type) will spawn on switch flag activation. After the specified
+ * timer amount, they unset the switch and disappear again until the flag is once again set.
+ *
  *  Default (Rot X = 0) timer is 10 seconds. You can go up to 10 minutes (600).
- * 
+ *
  *  Square platforms = 200 units
  *  Hex platforms    = 328 units
  *  Cone platform    = 206 units
-*/
+ */
 
 #define FLAGS (ACTOR_FLAG_4)
 
 #define SECONDS_TO_FRAMES(sec) (sec * (60 / R_UPDATE_RATE))
-static u8 sPlayerOn = false;
 static u16 sTimer = 0;
 
 void Platforms_Init(Actor* thisx, PlayState* play);
@@ -58,8 +57,6 @@ void Platforms_Disappear(Platforms* this, PlayState* play);
 
 void Platforms_SetupConeFloat(Platforms* this, PlayState* play);
 void Platforms_ConeFloat(Platforms* this, PlayState* play);
-void Platforms_SetupConeDip(Platforms* this, PlayState* play);
-void Platforms_ConeDip(Platforms* this, PlayState* play);
 
 void Platforms_DoNothing(Platforms* this, PlayState* play);
 
@@ -86,7 +83,6 @@ const ActorInit Platforms_InitVars = {
     (ActorFunc)Platforms_Update,
     (ActorFunc)Platforms_Draw,
 };
-
 
 static InitChainEntry sInitChain[] = {
     ICHAIN_VEC3F_DIV1000(scale, 100, ICHAIN_CONTINUE),
@@ -136,16 +132,14 @@ void Platforms_Init(Actor* thisx, PlayState* play) {
 
     this->dyna.actor.shape.rot.x = this->dyna.actor.home.rot.x = 0.0f;
     this->dyna.actor.shape.rot.z = this->dyna.actor.home.rot.z = 0.0f;
-    
+
     Actor_SetScale(&this->dyna.actor, 0.035f);
 
     if (PLATFORM_TYPE(this) != PLATFORM_TYPE_CONE_GRASS) {
         Platforms_SetupWaitForSwitch(this, play);
     } else {
-        this->coneDipPlayer = false;
-        this->coneDipReturn = false;
         Platforms_SetupConeFloat(this, play);
-    } 
+    }
 }
 
 void Platforms_Destroy(Actor* thisx, PlayState* play) {
@@ -227,7 +221,7 @@ void Platforms_Draw(Actor* thisx, PlayState* play) {
         drawFunc = Gfx_DrawDListXlu;
         // Gfx_DrawDListXlu(play, dlist);
     }
-    
+
     if (this->alpha > 0) {
         switch (PLATFORM_TYPE(this)) {
             case PLATFORM_TYPE_SQ_WOOD:
@@ -256,9 +250,8 @@ void Platforms_Draw(Actor* thisx, PlayState* play) {
                 break;
         }
     }
-    
-    CLOSE_DISPS(play->state.gfxCtx, __FILE__, __LINE__);
 
+    CLOSE_DISPS(play->state.gfxCtx, __FILE__, __LINE__);
 }
 
 void Platforms_SetupWaitForSwitch(Platforms* this, PlayState* play) {
@@ -293,59 +286,16 @@ void Platforms_SetupConeFloat(Platforms* this, PlayState* play) {
 }
 
 void Platforms_ConeFloat(Platforms* this, PlayState* play) {
-    this->dyna.actor.world.pos.y = this->dyna.actor.home.pos.y + 20.0f * (Math_SinS(play->gameplayFrames * 0x200)); // float up and down
+    f32 dipTarget = 0.0f;
 
-
-    if (DynaPolyActor_IsPlayerAbove(&this->dyna)) {
-        this->coneDipPlayer = false;
-        if (DynaPolyActor_IsPlayerOnTop(&this->dyna) && !this->playerOn) {
-            this->playerOn = true;
-            Math_Vec3f_Copy(&this->targetPos, &this->dyna.actor.world.pos);
-            this->targetPos.y -= 35.0f;
-            Platforms_SetupConeDip(this, play);
-            return;
-        }
-    } else if (!DynaPolyActor_IsPlayerOnTop(&this->dyna)) {
-        this->playerOn = false;
+    if (DynaPolyActor_IsPlayerOnTop(&this->dyna)) {
+        dipTarget = -35.0f;
     }
+
+    Math_SmoothStepToF(&this->dipOffset, dipTarget, 0.1f, 2.0f, 0.1f);
+    this->dyna.actor.world.pos.y =
+        (this->dyna.actor.home.pos.y + this->dipOffset) + 20.0f * (Math_SinS(play->gameplayFrames * 0x200)); // float up and down
 }
-
-void Platforms_SetupConeDip(Platforms* this, PlayState* play) {
-    this->coneDipPlayer = true;
-    this->coneDipReturn = false;
-    this->actionFunc = Platforms_ConeDip;
-}
-
-void Platforms_ConeDip(Platforms* this, PlayState* play) {
-    if (!DynaPolyActor_IsPlayerOnTop(&this->dyna)) {
-        this->playerOn = false;
-        Math_ApproachF(&this->dyna.actor.world.pos.y, this->dyna.actor.home.pos.y, 0.1f, 5.0f);
-        if (this->dyna.actor.world.pos.y >= this->dyna.actor.home.pos.y) {
-            this->coneDipReturn = false;
-            Platforms_SetupConeFloat(this, play);
-            return;
-        }
-    }
-    
-    if (this->coneDipPlayer) {
-        Math_ApproachF(&this->dyna.actor.world.pos.y, this->targetPos.y, 0.1f, 5.0f);
-
-        if (this->dyna.actor.world.pos.y == this->targetPos.y) {
-            this->coneDipPlayer = false;
-            this->coneDipReturn = true;
-        }
-    } else {
-        if (this->coneDipReturn) {
-            Math_ApproachF(&this->dyna.actor.world.pos.y, this->dyna.actor.home.pos.y, 0.1f, 5.0f);
-            if (this->dyna.actor.world.pos.y == this->dyna.actor.home.pos.y) {
-                this->coneDipReturn = false;
-                Platforms_SetupConeFloat(this, play);
-            }
-        }
-    }
-}
-
 
 void Platforms_DoNothing(Platforms* this, PlayState* play) {
-
 }
